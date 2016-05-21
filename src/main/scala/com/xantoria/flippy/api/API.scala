eba70f9e92176835e99a37f5fbe5aa5c92545dd8
Rob Moore <giftiger.wunsch@xantoria.com>
2016-05-21 21:17:19 +0100

    Make API error handling more generic

--- src/main/scala/com/xantoria/flippy/api/API.scala
+++ src/main/scala/com/xantoria/flippy/api/API.scala
@@ -2,11 +2,12 @@ package com.xantoria.flippy.api
 
 import scala.concurrent.ExecutionContext
 import scala.util.{Success, Failure}
+import scala.util.control.NonFatal
 
 import akka.actor.Actor
 import net.liftweb.json._
 import spray.httpx.LiftJsonSupport
-import spray.routing.HttpService
+import spray.routing.{ExceptionHandler, HttpService}
 
 import com.xantoria.flippy.condition.Condition
 import com.xantoria.flippy.db.Backend
@@ -38,21 +39,23 @@ trait APIHandling extends HttpService with LiftJsonSupport {
   protected val backend: Backend
   protected implicit val ec: ExecutionContext
 
+  protected implicit def exceptionHandler = ExceptionHandler {
+    case NonFatal(e) => complete(500 -> ErrorMessage(e))
+  }
+
   def handleSwitch = pathPrefix("switch" / Segment) {
     switchName: String => {
       pathEndOrSingleSlash {
         post {
           entity(as[Condition]) {
-            cond => onComplete(backend.configureSwitch(switchName, cond)) {
-              case Success(_) => complete(200 -> InfoMessage())
-              case Failure(e) => complete(500 -> s"An internal error occurred: ${e.getMessage}")
+            cond => onSuccess(backend.configureSwitch(switchName, cond)) {
+              _ => complete(200 -> InfoMessage())
             }
           }
         } ~
         get {
-          onComplete(backend.switchConfig(switchName)) {
-            case Success(cond) => complete(200 -> cond)
-            case Failure(e) => complete(500 -> ErrorMessage(e))
+          onSuccess(backend.switchConfig(switchName)) {
+            cond => complete(200 -> cond)
           }
         } ~
         put {
@@ -65,9 +68,8 @@ trait APIHandling extends HttpService with LiftJsonSupport {
       path("is_active" ~ Slash.?) {
         post {
           entity(as[Map[String, ContextValue]]) {
-            data => onComplete(backend.isActiveSafe(switchName, data mapValues { _.underlying })) {
-              case Success(b) => complete(200 -> IsActive(b))
-              case Failure(e) => complete(500 -> ErrorMessage(e))
+            data => onSuccess(backend.isActiveSafe(switchName, data mapValues { _.underlying })) {
+              b => complete(200 -> IsActive(b))
             }
           }
         }
@@ -78,11 +80,10 @@ trait APIHandling extends HttpService with LiftJsonSupport {
   def handleSwitchListing = path("switch" ~ Slash.?) {
     get {
       parameters("offset".as[Option[Int]]) {
-        offset => onComplete(backend.listSwitches(offset = offset, limit = Some(10))) {
-          case Success(data) => complete(
+        offset => onSuccess(backend.listSwitches(offset = offset, limit = Some(10))) {
+          data => complete(
             200 -> data.map { s => SwitchConfig(s._1, s._2) }
           )
-          case Failure(e) => complete(500 -> ErrorMessage(e))
         }
       }
     }
