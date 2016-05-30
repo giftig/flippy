f57dba3e2344acce14389c048030f85eee274c52
Rob Moore <giftiger.wunsch@xantoria.com>
2016-05-30 18:24:11 +0100

    Web interface: add pagination, add/delete switches
    Deletion won't work until I implement the delete API call, of course.

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,4 +1,5 @@
 (function($) {
+  var DEFAULT_CONDITION = {condition_type: 'False'};
   var $errorBox;
 
   // Controls for building a switch
@@ -45,21 +46,30 @@
           self.updateSwitch(data, success, err);
         });
 
-      var $cancelButton = $('<button>')
-        .text('Cancel')
-        .addClass('cancel')
-        .attr('data-action', 'cancel')
-        .click(function() {
-          var $conf = $(this).parents('.switch').find('.config');
-          $conf.val($conf.text());
-        });
+      var $cancelButton = $('<button>').text('Cancel').addClass('cancel');
+      $cancelButton.click(function() {
+        var $conf = $(this).parents('.switch').find('.config');
+        $conf.val($conf.text());
+      });
 
-      $persistControls.append($saveButton).append($cancelButton);
+      var $deleteButton = $('<button>').text('Delete').addClass('delete');
+      $deleteButton.click(function() {
+        if(!confirm('Are you sure you want to delete the ' + self.name + ' switch?')) {
+          return;
+        }
+
+        self.deleteSwitch();
+      });
+
+      $persistControls.append($saveButton).append($cancelButton).append($deleteButton);
       $controls.append($persistControls);
       return $controls;
     };
 
     self.updateSwitch = function(switchContent, success, err) {
+      success = success || function() {};
+      err = err || function() {};
+
       try {
         self.condition = JSON.parse(switchContent);
       } catch(e) {
@@ -74,10 +84,23 @@
         data: JSON.stringify(self.condition),
         dataType: 'json',
         contentType: 'application/json',
-        complete: success,
+        success: success,
         error: function(resp) {
           self.displayError(resp.responseText, resp.status + ' ' + resp.statusText);
           err();
+        }
+      });
+    };
+
+    self.deleteSwitch = function() {
+      $.ajax({
+        url: self.baseUrl + 'switch/' + self.name + '/',
+        type: 'DELETE',
+        success: function() {
+          self.admin.switchDeleted(self.name);
+        },
+        error: function(resp) {
+          self.displayError(resp.responseText, resp.status + ' ' + resp.statusText);
         }
       });
     };
@@ -108,24 +131,72 @@
       $(this).hide();
     });
 
-    self.listSwitches = function() {
+    self.listSwitches = function(offset, cb) {
+      offset = offset || 0;
+
       $.ajax({
-        url: self.baseUrl + 'switch/',
+        url: self.baseUrl + 'switch/?offset=' + offset,
         type: 'GET',
         success: function(data) {
           self.renderSwitches(data);
         },
         error: function(data) {
           self.displayError(data);
-        }
+        },
+        complete: cb
       });
     };
 
     self.renderSwitches = function(switches) {
-      self.$container.empty();
       for (var i = 0; i < switches.length; i++) {
-        self.$container.append(new SwitchBuilder(switches[i], self).render());
+        self.$switchList.append(new SwitchBuilder(switches[i], self).render());
       }
+    };
+
+    // Render the controls for new switches etc.
+    self.renderControls = function() {
+      // HEADER CONTROLS
+      var $newSwitch = $('<button>').addClass('new-switch').text('New switch');
+      var $newSwitchControls = $('<span>').addClass('new-switch-controls').addClass('extended');
+      var $nameBox = $('<input>').addClass('name').val('new_switch');
+      var $confirmButton = $('<button>').addClass('confirm').text('Create');
+      $newSwitchControls.html($nameBox).append($confirmButton);
+
+      $newSwitch.click(function() {
+        $newSwitchControls.show();
+        $newSwitch.hide();
+      });
+      $confirmButton.click(function() {
+        var name = $nameBox.val();
+        $nameBox.val('new_switch');
+
+        if (self.$container.find('.switch[data-name="' + name + '"]').length !== 0) {
+          self.displayError('A switch with that name already exists.', 'Bad switch name');
+          return;
+        }
+
+        var newSwitch = new SwitchBuilder(
+          {name: name, condition: DEFAULT_CONDITION}, self
+        );
+        self.$switchList.prepend(newSwitch.render());
+        $newSwitchControls.hide();
+        $newSwitch.show();
+      });
+      self.$headerControls.html($newSwitch);
+      self.$headerControls.append($newSwitchControls);
+
+      // FOOTER CONTROLS
+      $showMore = $('<button>').addClass('show-more').text('Show more...');
+      $showMore.click(function() {
+        $showMore.attr('disabled', true);
+        var offset = self.$container.find('.switch').length;
+
+        self.listSwitches(offset, function() {
+          $showMore.attr('disabled', false);
+        });
+      });
+      self.$footerControls.html($showMore);
+
     };
 
     // TODO: Make this nicer
@@ -137,6 +208,25 @@
       }
       self.$errorBox.append($('<span>').text(msg));
       self.$errorBox.show();
+    };
+
+    self.render = function() {
+      self.$container.empty();
+      self.$headerControls = $('<div>').addClass('controls');
+      self.$switchList = $('<div>').addClass('switches');
+      self.$footerControls = $('<div>').addClass('controls');
+
+      self.$container.append(self.$headerControls);
+      self.$container.append(self.$switchList)
+      self.$container.append(self.$footerControls);
+
+      self.renderControls();
+      self.listSwitches();
+    };
+
+    // Called to indicate a switch has been removed
+    self.switchDeleted = function(name) {
+      self.$container.find('.switch[data-name="' + name + '"]').remove();
     };
   };
 
