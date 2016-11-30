412fcd1623eff3419df61f58d020a4f2223d1656
Rob Moore <giftiger.wunsch@xantoria.com>
2016-11-30 22:40:49 +0000

    Branched redis support
    Refactor out the underlying methods into a wrapped methods with a trait,
    which apply the namespace for you and are abstract in the underlying
    trait.
    
    This will make it nice and easy to implement a version of the redis
    backend which uses a hash instead of a load of prefixed keys by simply
    overriding the methods defined in RedisSupport

--- src/main/scala/com/xantoria/flippy/db/RedisBackend.scala
+++ src/main/scala/com/xantoria/flippy/db/RedisBackend.scala
@@ -10,6 +10,28 @@ import org.slf4j.LoggerFactory
 
 import com.xantoria.flippy.condition.Condition
 
+/**
+ * Defines methods for handling fetching switches and configs from redis
+ *
+ * In particular, it contains abstract definitions for setting, getting, scanning, and mgetting
+ * keys either from the redis keyspace or from a hash, according to the concrete definition
+ * imposed by the implementing backend variant. (i.e. using either GET, SET, MGET, SCAN or
+ * HGET, HSET, HMGET, HSCAN
+ */
+trait RedisSupport {
+  protected def rget(key: String)(implicit c: RedisClient): Option[String]
+  protected def rset(key: String, data: String)(implicit c: RedisClient): Boolean
+  protected def rdel(key: String)(implicit c: RedisClient): Option[Long]
+
+  // Yo dawg, I herd you liek options...
+  protected def rscan(cursor: Int, count: Int = 10)(implicit c: RedisClient): Option[
+    (Option[Int], Option[List[Option[String]]])
+  ]
+  protected def rmget(key: String, keys: String*)(
+    implicit c: RedisClient
+  ): Option[List[Option[String]]]
+}
+
 class RedisBackend(
   host: String,
   port: Int,
@@ -18,12 +40,31 @@ class RedisBackend(
 )(
   implicit val ec: ExecutionContext,
   implicit val formats: Formats
-) extends Backend {
+) extends Backend with RedisSupport {
   private final val ALLOWED_SWITCH_PATTERN = """^[\w-_]{1,64}$""".r
   private val logger = LoggerFactory.getLogger(classOf[RedisBackend])
 
   logger.info(s"Using redis backend at $host:$port")
-  private def client = new RedisClient(host, port)
+  protected def client = new RedisClient(host, port)
+
+  // Use namespace as a prefix to provide a concrete implementation of RedisSupport methods
+  protected def rget(key: String)(implicit c: RedisClient): Option[String] = {
+    c.get[String](s"$namespace:$key")
+  }
+  protected def rset(key: String, data: String)(implicit c: RedisClient): Boolean = {
+    c.set(s"$namespace:$key", data)
+  }
+  protected def rdel(key: String)(implicit c: RedisClient): Option[Long] = c.del(
+    s"$namespace:$key"
+  )
+  protected def rscan(cursor: Int, count: Int = 10)(implicit c: RedisClient): Option[
+    (Option[Int], Option[List[Option[String]]])
+  ] = c.scan[String](cursor, s"$namespace:*", count)
+  protected def rmget(key: String, keys: String*)(
+    implicit c: RedisClient
+  ): Option[List[Option[String]]] = {
+    c.mget[String](s"$namespace:$key", (keys map { k: String => s"$namespace:$k" }): _*)
+  }
 
   /**
    * Make sure the switch name provided is acceptable for this backend, or throw an exception
@@ -45,7 +86,7 @@ class RedisBackend(
     logger.info(s"Getting config for switch $name")
     validateName(name)
 
-    c.get(s"$namespace:$name") map {
+    rget(name)(c) map {
       parseJson(_).extract[Condition]
     }
   }
@@ -53,15 +94,13 @@ class RedisBackend(
   /**
    * Get a whole batch of switch configs with a single redis MGET call
    */
-  private def switchConfigs(names: String*)(c: RedisClient): Future[
+  private def switchConfigs(names: String*)(implicit c: RedisClient): Future[
     List[Option[Condition]]
   ] = Future {
     logger.info(s"Getting config for a batch of ${names.length} switches")
 
-    val keys = names.map { name => s"$namespace:$name" }
-
-    keys match {
-      case head :: tail => c.mget(head, tail: _*) map {
+    names match {
+      case head :: tail => rmget(head, tail: _*) map {
         results: List[Option[String]] => {
           results map {
             _.map {
@@ -74,10 +113,9 @@ class RedisBackend(
     }
   }
 
-
   def deleteSwitch(name: String): Future[Unit] = Future {
     logger.info(s"Deleting switch $name")
-    client.del(s"$namespace:$name")
+    rdel(name)(client)
   }
 
   def configureSwitch(name: String, condition: Condition): Future[Unit] = Future {
@@ -85,7 +123,7 @@ class RedisBackend(
     validateName(name)
 
     val data = writeJson(condition)
-    client.set(s"$namespace:$name", data)
+    rset(name, data)(client)
   }
   def switchConfig(name: String): Future[Option[Condition]] = _switchConfig(name, client)
 
@@ -97,13 +135,10 @@ class RedisBackend(
    */
   def listSwitches(offset: Option[Int], limit: Option[Int]): Future[List[(String, Condition)]] = {
     logger.info("Scanning redis for switches")
-    val c = client
+    implicit val c = client
 
     def fetchKeys(cursor: Int = 0, acc: List[String] = Nil): Future[List[String]] = {
-      // Yo dawg, I herd you liek Options...
-      val res: Future[Option[(Option[Int], Option[List[Option[String]]])]] = Future {
-        c.scan[String](cursor, s"$namespace:*")
-      }
+      val res: Future[Option[(Option[Int], Option[List[Option[String]]])]] = Future(rscan(cursor))
 
       res flatMap {
         _ map {
@@ -133,7 +168,7 @@ class RedisBackend(
     // TODO: Use a dedicated future pool for the Future.traverse here
     switches flatMap {
       keys: List[String] => Future.traverse(keys.grouped(batchRetrievalSize).toList) {
-        batch: List[String] => switchConfigs(batch: _*)(client) map {
+        batch: List[String] => switchConfigs(batch: _*) map {
           results => (batch zip results) collect {
             // Prevents a None.get in case of race conditions, but a None shouldn't
             // generally exist for the result
